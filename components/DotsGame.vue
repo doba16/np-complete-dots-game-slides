f9f2515b3ac592d5ec41316eb4aa005cc5a23b2a
doba16 <92383352+doba16@users.noreply.github.com>
2023-03-31 13:36:17 +0200

    INIT

---
+++ components/DotsGame.vue
@@ -0,0 +1,25 @@
+<template>
+    <canvas ref="testCanvasRef"></canvas>
+</template>
+
+<script setup>
+import { DotsGame } from "@doba16/predictable-dots-game"
+import { ref, onMounted } from "vue";
+import "./canvas-style.css"
+
+// IMPORTANT: Variable should be named the same as the ref.
+const testCanvasRef = ref(null);
+
+onMounted(() => {
+    console.log("Mein tolles Canvas:", testCanvasRef)
+
+    new DotsGame({
+        canvas: testCanvasRef.value,
+        width: 6,
+        height: 6
+    }).startDrawing()
+
+    console.log("Started drawing...")
+});
+
+</script>
